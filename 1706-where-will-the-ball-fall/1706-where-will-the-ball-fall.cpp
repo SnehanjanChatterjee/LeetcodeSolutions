4c7ed924518570e8b692202752242386bb2455ab
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-11-02 03:07:53 +0530

    Time: 31 ms (83.74%), Space: 13.2 MB (81.02%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,7 +1,6 @@
 class Solution {
 private:
-    int rows, cols;
-    int dfs(int r, int c, vector<vector<int>>& grid) {
+    int dfs(int r, int c, int& rows, int& cols, vector<vector<int>>& grid) {
         // When the ball actually leaves the grid, the column it's in will be the ans
         if(r == rows) return c;
         
@@ -9,13 +8,13 @@ private:
             // If a cell is '\' then check if its right cell is also '\' or not. Otherwise it'll get stuck
             if(grid[r][c] == 1) {
                 if(r<rows && c+1<cols && r>=0 && c+1>=0 && grid[r][c+1]==1) {
-                    return dfs(r+1, c+1, grid);
+                    return dfs(r+1, c+1, rows, cols, grid);
                 }
             }
             // If a cell is '/' then check if its left cell is also '/' or not. Otherwise it'll get stuck
             else if(grid[r][c] == -1) {
                 if(r<rows && c-1<cols && r>=0 && c-1>=0 && grid[r][c-1]==-1) {
-                    return dfs(r+1, c-1, grid);
+                    return dfs(r+1, c-1, rows, cols, grid);
                 }
             }
         }
@@ -24,10 +23,10 @@ private:
     }
 public:
     vector<int> findBall(vector<vector<int>>& grid) {
-        rows = grid.size(), cols = grid[0].size();
+        int rows = grid.size(), cols = grid[0].size();
         vector<int> ans(cols, -1);
         for(int c=0; c<cols; c++) {
-            ans[c] = dfs(0, c, grid);
+            ans[c] = dfs(0, c, rows, cols, grid);
         }
         return ans;
     }
