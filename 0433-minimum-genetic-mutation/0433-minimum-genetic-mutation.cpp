5f64484d6b1fa99af0e11c6b155004f9b272a44b
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-11-02 21:56:33 +0530

    Time: 4 ms (29.93%), Space: 6.8 MB (15.87%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -3,7 +3,6 @@ public:
     int minMutation(string start, string end, vector<string>& bank) {
         int step = 0;
         unordered_set<string> us(bank.begin(), bank.end());
-        vector<string> forErase;
         queue<string> q;
         
         q.push(start);
@@ -14,7 +13,6 @@ public:
                 q.pop();
 
                 if(original == end) {
-                    // cout<<"Found end = "<<end<<", step = "<<step<<endl;
                     return step;
                 }
 
@@ -22,25 +20,12 @@ public:
                 
                 for(int i=0; i<original.size(); i++) {
                     string mutated = original;
-                    // if(start[i] != end[i]) {
-                        for(char ch: {'A', 'C', 'G', 'T'}) {
-                            mutated[i] = ch;
-                            // cout<<original<<" Mutated to "<<mutated<<endl;
-                            // for(int s=0; s<bank.size(); s++) {
-                            //     if(bank[s] == mutated) {
-                            //         q.push(mutated);
-                            //         // step++;
-                            //         cout<<mutated<<" Found in bank, step = "<<step<<endl;
-                            //     }
-                            // }
-                            if(us.find(mutated) != us.end()) {
-                                q.push(mutated);
-                                // cout<<mutated<<" Found in bank, step = "<<step<<endl;
-                            }
+                    for(char ch: {'A', 'C', 'G', 'T'}) {
+                        mutated[i] = ch;
+                        if(us.find(mutated) != us.end()) {
+                            q.push(mutated);
                         }
-                        // mutated = original;
-                    // }
-
+                    }
                 }
             }
             step++;
