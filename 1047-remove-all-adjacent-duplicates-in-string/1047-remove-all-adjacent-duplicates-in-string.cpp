4e799632506648927ce9f9277f3f2b6888b017c1
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-11-10 18:12:53 +0530

    Time: 61 ms (47.07%), Space: 11.6 MB (13.06%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:
-    string removeDuplicates(string s) {
+private:
+    string soln1(string s) {
         int size = s.size();
         for(int i=0; i<size-1; i++) {
             if(s[i] == s[i+1]) {
@@ -11,5 +11,25 @@ public:
             }
         }
         return s;
+    }
+    
+    string soln2(string s) {
+        int size = s.size();
+        string ans;
+        for(char &ch: s) {
+            if(ans.size() == 0 || ans.back() != ch) {
+                ans.push_back(ch);
+            }
+            else if(ans.back() == ch) {
+                ans.pop_back();
+            }
+        }
+        return ans;
+    }
+public:
+    string removeDuplicates(string s) {
+        // return soln1(s);
+        
+        return soln2(s);
     }
 };
