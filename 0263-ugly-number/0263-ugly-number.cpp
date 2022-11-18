0fbe28eb771d65dbb71683492ce0dd2722a961b1
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-11-18 22:29:57 +0530

    Time: 36 ms (5.67%), Space: 5.7 MB (98.89%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n == 0) return false;
+        if (n < 1) return false;
         for(int i=2; i<=sqrt(n); i++) {
             if(n % i == 0 && i > 5) return false;
             while(n % i == 0) n /= i;
