d923efd4988cfb16cc199f6e770d112e9842fd90
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-11-18 22:25:47 +0530

    Time: 6 ms (26.78%), Space: 6 MB (19.97%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n == 0) return false;
+        for(int i=2; i<=sqrt(n); i++) {
+            if(n % i == 0 && i > 5) return false;
+            while(n % i == 0) n /= i;
+        }
+        if(n > 5 || n < 0) return false;
+        return true;
+    }
+};
