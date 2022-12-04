8a9144646e08bf93d7a2c59a72d34f4f1522d770
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-12-05 01:53:23 +0530

    Time: 156 ms (76.57%), Space: 91 MB (19.31%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -8,18 +8,11 @@ public:
         suffixSum[n-1] = nums[n-1];
         for(int i = n-2; i>=0; i--) {
             suffixSum[i] = nums[i] + suffixSum[i+1];
-            // cout<<"suffixSum["<<i<<"] = "<<suffixSum[i]<<endl;
         }
-        
-        // if(n-2 > 0 ) suffixSum[n-2] = nums[n-1];
-        // for(int i = n-3; i>=0; i--) {
-        //     suffixSum[i] = nums[i+1] + suffixSum[i+2];
-        // }
         
         prefixSum[0] = nums[0];
         for(int i=1; i<n; i++) {
             prefixSum[i] = nums[i] + prefixSum[i-1];
-            // cout<<"prefixSum["<<i<<"] = "<<prefixSum[i]<<endl;
         }
         
         int ans = INT_MAX;
@@ -36,8 +29,6 @@ public:
                 minDiff = min(minDiff, absDiff);
                 ans = i;
             }
-            
-            // cout<<"For "<<i<<"th no = "<<nums[i]<<" firstHalfAvg = "<<firstHalfAvg<<", secondHalfAvg = "<<secondHalfAvg<<", ans = "<<ans<<endl;
         }
         
         return ans;
