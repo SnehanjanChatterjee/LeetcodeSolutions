f6143a4c01d61a257510c98e4ee1ba0a0fef3d37
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-12-13 20:07:33 +0530

    Time: 32 ms (55.07%), Space: 9.8 MB (95.85%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -7,6 +7,10 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int rows = matrix.size(), cols = matrix[0].size();
         
+        // Starting from the 2nd row, for each cell, update its value as the min of following 3:
+        // 1. matrix[r][c] + matrix[r-1][c-1]
+        // 2. matrix[r][c] + matrix[r-1][c]
+        // 3. matrix[r][c] + matrix[r-1][c+1]
         for(int r=1; r<rows; r++) {
             for(int c=0; c<cols; c++) {
                 int minSum = INT_MAX;
@@ -23,6 +27,7 @@ public:
             }
         }
         
+        // Now our answer will be the min value from the last row 
         return *min_element(matrix[rows-1].begin(), matrix[rows-1].end());
     }
 };
