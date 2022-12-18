0e157530810f298ad6084918a77387bb5cae8687
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-12-18 14:46:43 +0530

    Time: 437 ms (46.40%), Space: 98.7 MB (74.20%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -5,18 +5,23 @@ public:
         stack<pair<int, int>> st;
         int n = temperatures.size();
         vector<int> ans(n);
-        for(int i=n-1; i>=0; i--) {
-            int cur = temperatures[i];
+        
+        for(int curInd = n - 1; curInd >= 0; curInd--) {
+            int curTemp = temperatures[curInd];
             if(!st.empty()) {
-                while(!st.empty() && st.top().first <= cur) st.pop();
+                // Till we dont find a temp > current temp keep popping stack elements
+                while(!st.empty() && st.top().first <= curTemp) {
+                    st.pop();
+                }
+                
+                // Check after popping all elements if stack still has any element
+                // If it has that must a temp > current temp
                 if(!st.empty()) {
-                    auto itr = st.top();
-                    int reqIndex = itr.second;
-                    ans[i] = reqIndex - i;
-                    
+                    int reqInd = st.top().second;
+                    ans[curInd] = reqInd - curInd;
                 }
             }
-            st.push({cur, i});
+            st.push({curTemp, curInd});
         }
         
         return ans;
