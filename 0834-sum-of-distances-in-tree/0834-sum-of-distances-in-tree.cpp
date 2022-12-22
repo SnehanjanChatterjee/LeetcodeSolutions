97caa9beac04398b117c3aef9a04242ee8726e87
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-12-22 14:32:10 +0530

    Time: 272 ms (94.61%), Space: 87.5 MB (72.36%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     // https://leetcode.com/problems/sum-of-distances-in-tree/discuss/1308366/C%2B%2B-solution-using-dfs-with-explanation-O(N)-time-complexity
-    vector<vector<int>> v;
+    vector<vector<int>> adj;
     vector<int> counter, res;
         
     void dfs(int i, int p = -1) {
-        for(auto u : v[i]) {
+        for(auto u : adj[i]) {
             if(u == p) continue;
             dfs(u, i);
             counter[i] += counter[u];
@@ -15,7 +15,7 @@ public:
     }
 
     void dfs2(int i, int n, int p = -1) {
-        for(auto u : v[i]) {
+        for(auto u : adj[i]) {
             if(u == p) continue;
             res[u] = res[i] - counter[u] + n - counter[u];
             dfs2(u, n, i);
@@ -23,12 +23,12 @@ public:
     }
     
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        v.resize(n);
+        adj.resize(n);
         for(int i = 0; i < n - 1; i++) {
             int a = edges[i][0];
             int b = edges[i][1];
-            v[a].push_back(b);
-            v[b].push_back(a);
+            adj[a].push_back(b);
+            adj[b].push_back(a);
         }
         res.resize(n);
         counter.resize(n);
