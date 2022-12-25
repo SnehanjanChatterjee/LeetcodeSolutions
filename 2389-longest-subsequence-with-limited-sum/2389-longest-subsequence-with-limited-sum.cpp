a6584c7a043cce01cf0469fb970a6c1e456d5f2c
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-12-25 14:27:50 +0530

    Time: 26 ms (82.20%), Space: 13.9 MB (36.20%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:
-    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+private:
+    vector<int> soln1(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(), nums.end());
         int q = queries.size();
         int n = nums.size();
@@ -28,5 +28,29 @@ public:
         }
         
         return ans;
+    }
+    
+    // https://leetcode.com/problems/longest-subsequence-with-limited-sum/discuss/2493409/JavaC%2B%2BPython-Binary-Search
+    // Explanation:
+    // 1. Sort input A, since we care the sum and doesn't care the order.
+    // 2. Calculate the prefix sum of A, since we want to know the accumulate sum.
+    // 3. Binary seach each query q in query, and the index is the result.
+    // 4. return result res.
+    vector<int> soln2(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(), nums.end());
+        vector<int> res;
+        for (int i = 1; i < nums.size(); ++i) {
+            nums[i] += nums[i - 1];
+        }
+        for (int& q: queries) {
+            res.push_back(upper_bound(nums.begin(), nums.end(), q) - nums.begin());
+        }
+        return res;
+    }
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        // return soln1(nums, queries);
+        
+        return soln2(nums, queries);
     }
 };
