48a9abf384a23438a5738b5a60487de089eea213
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-12-26 22:03:35 +0530

    Time: 60 ms (97.09%), Space: 48.5 MB (48.80%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -13,9 +13,12 @@ private:
         return false;
     }
     
+    // https://www.youtube.com/watch?v=muDPTDrpS28
     bool soln2(vector<int>& nums) {
         int maxReachableInd = 0, n = nums.size();
         
+        // We carry a maxReachableInd which denotes the max index to which we can reach at any point.
+        // If i cross maxReachableInd that means we encountered 0 and we couldn't go beyond that so we should stop
         for(int i=0; i<n && maxReachableInd<n && i<=maxReachableInd; i++) {
             maxReachableInd = max(maxReachableInd, i + nums[i]);
         }
