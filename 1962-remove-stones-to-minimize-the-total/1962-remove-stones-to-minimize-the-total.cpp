85d7e7fe43c99d8f13627f97d9ff13a0e0422015
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-12-28 14:08:03 +0530

    Time: 538 ms (93.45%), Space: 105 MB (55.34%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -4,17 +4,24 @@ public:
         priority_queue<int> pq;
         int sum = 0;
         
+        // Push all piles into priority queue such that the max pile appear at top
+        // Calculate sum also
         for(int &pile: piles) {
             pq.push(pile);
             sum += pile;
         }
         
+        // At each iteration fetch fetch pile (i.e. top of priority queue) and pop it
+        // Remove stones from that pile and push again into priority queue
+        // Modify sum along with this
         while(k--) {
-            int oldNum = pq.top();
-            int newNum = oldNum - (oldNum / 2);
-            sum = sum - oldNum + newNum;
+            int oldPile = pq.top();
+            int newPile = oldPile - (oldPile / 2);
+            
+            sum = sum - oldPile + newPile;
+            
             pq.pop();
-            pq.push(newNum);
+            pq.push(newPile);
         }
         
         return sum;
