93196e43137bad575a73925f9c55e88b8c705464
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-01-08 14:00:49 +0530

    Time: 134 ms (30.11%), Space: 47.4 MB (5.05%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        // unordered_map<double, set<pair<int, int>>> ump;
         int rows = points.size();
         int cols = points[0].size();
         int ans = 1;
@@ -17,21 +16,14 @@ public:
                     double dy = (y1 - y2);
                     double slope = (dx == 0) ? INT_MAX : (double)(dy / dx);
                                  
-                    // if(slope == -0) slope = 0;
-                    // ump[slope]++;
                     ump[slope].insert({x1, y1});
-                    ump[slope].insert({x2, y2});
-                                 
-                    // cout<<"B/W ("<<points[i][0]<<","<< points[i][1]<<") && ("<<points[j][0]<<","<<points[j][1]<<") xDiff = "<<xDiff<<" yDiff = "<<yDiff<<" slope is "<<slope<<endl;
-                                 
+                    ump[slope].insert({x2, y2});         
                 }
             }
             
-            for(auto itr: ump) {
+            for(auto &itr: ump) {
                 int setSize = itr.second.size();
                 ans = max(ans, setSize);
-                // cout<<"For slope = "<<itr.first<<" coordinates are: "<<endl;
-                // for(auto x: itr.second) cout << "(" << x.first <<","<< x.second << ")" << endl;
             }
         }
         
