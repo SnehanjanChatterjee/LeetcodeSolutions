e7b8c7e5939981fe63fb42951972ed47cc780472
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-01-17 00:42:30 +0530

    Time: 19 ms (74.71%), Space: 17.2 MB (45.20%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    // https://leetcode.com/problems/insert-interval/discuss/1327206/99.16-faster-or-proper-explainationor-c%2B%2B-solutionor-O(N)
+   vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size(), i = 0;
+        vector<vector<int>> res;
+        //case 1: no overlapping case before the merge intervals
+		//compare ending point of intervals to starting point of newInterval
+        while(i < n && intervals[i][1] < newInterval[0]){
+            res.push_back(intervals[i]);
+            i++;
+        }                           
+		//case 2: overlapping case and merging of intervals
+        while(i < n && newInterval[1] >= intervals[i][0]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.push_back(newInterval);
+        // case 3: no overlapping of intervals after newinterval being merged
+        while(i < n){
+            res.push_back(intervals[i]);
+            i++;
+        }
+        return res;
+    }
+};
