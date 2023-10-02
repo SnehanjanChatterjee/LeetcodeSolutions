2599734172800972c9a9c69c82b59d03434d7573
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-10-03 00:21:22 +0530

    Time: 8 ms (85.83%), Space: 17.7 MB (19.70%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool hasGroupsSizeX(vector<int>& deck) {
-        unordered_map<int, int> mp;
-        
-        for(auto &x: deck) {
-            mp[x]++;
-        }
-        
-        int count = 0;
-        for(auto i: mp) {
-            count = __gcd(i.second, count);
-        }
-        
-        return (count >=2);
+        unordered_map<int, int> m;     // order doesn't matter, so we use unordered map
+        for(int d : deck)
+            m[d]++;    // map created with frequency of each card no.
+        int gcd = m[deck[0]];
+        for(auto i : m)
+            gcd = __gcd(gcd, i.second);    // doing gcd with the frequency of all the cards
+        return gcd != 1;    // if gcd is not equal to 1 it means we can create groups of size >= 2.
     }
 };
