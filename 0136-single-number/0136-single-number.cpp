96fe1dac4aeb12bcce8b3edb8635997aabbe5887
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-12-16 23:48:30 +0530

    Time: 11 ms (88.37%), Space: 17.2 MB (81.13%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0, n = nums.size();
+        for(int i = 0; i < n; i++) {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
