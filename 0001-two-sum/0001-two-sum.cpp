ea3a8b5464954fd74788d1a7907c2e028c374c36
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-12-17 02:52:09 +0530

    Time: 12 ms (60.96%), Space: 11.4 MB (20.20%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+private:
+    vector<int> solution1(vector<int>& nums, int target) {
         multimap<int, int> mp;
         int n = nums.size();
         
@@ -18,5 +18,33 @@ public:
             else if(i->first + j->first < target) i++;
         }
         return {};
+    }
+    
+    vector<int> solution2(vector<int>& nums, int target) {
+        int n = nums.size();
+        int i=0, j=n-1;
+        vector<int> ans(2);
+        
+        vector<pair<int, int>> nums2;
+        for(int i=0; i<n; i++) {
+            nums2.push_back({nums[i], i});
+        }
+        sort(nums2.begin(), nums2.end());
+        
+        while(i<j && i<n && j>=0) {
+            if(nums2[i].first + nums2[j].first == target) {
+                ans[0] = nums2[i].second;
+                ans[1] = nums2[j].second;
+                break;
+            }
+            else if(nums2[i].first + nums2[j].first < target) i++;
+            else if(nums2[i].first + nums2[j].first > target) j--;
+        }
+        return ans;
+    }
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // return solution1(nums, target);
+        return solution2(nums, target);
     }
 };
