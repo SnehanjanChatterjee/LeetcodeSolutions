9237424f75530d2c678de50d09af7c7e389bdb30
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-12-18 01:00:06 +0530

    Time: 3 ms (66.82%), Space: 12.5 MB (54.25%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+    // https://www.youtube.com/watch?v=JDOXKqF60RQ&list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB&index=11
     void nextPermutation(vector<int>& nums) {
         // next_permutation(nums.begin(), nums.end());
         
-        int n = nums.size();
-        int index1=-1, index2=-1;
-        for(int i=n-2; i>=0; i--) {
+        int n = nums.size(), index1 = -1, index2 = -1;
+        
+        // 1. Find index1 such that nums[i] < nums[i+1]
+        for(int i = n - 2; i >= 0; i--) {
             if(nums[i]<nums[i+1]) {
                 index1 = i;
                 // cout << "Found index1 = " << index1 << endl;
@@ -16,7 +18,9 @@ public:
             sort(nums.begin(), nums.end());
             // cout << "Not Found index1 = " << index1 << ", so sorted array" << endl;
         } else {
-            for(int i=n-1; i>index1; i--) {
+            
+            // 2. Find index2 toward right of index1 such that nums[i] > nums[index1]
+            for(int i = n - 1; i > index1; i--) {
                 if(nums[i]>nums[index1]) {
                     index2 = i;
                     // cout << "Found index2 = " << index2 << endl;
@@ -24,18 +28,12 @@ public:
                 }
             }
             
-            int temp = nums[index1];
-            nums[index1] = nums[index2];
-            nums[index2] = temp;
-
+            // 3. Swap nums[index1] & nums[index2]
+            swap(nums[index1], nums[index2]);
             // cout<<"Swapped nums[index1]= "<<nums[index1]<<" & nums[index2]= "<<nums[index2]<< endl;
 
-            for(int i=index1+1, j=n-1;i<j;i++, j--) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                // cout<<"Swapped nums[i]= "<<nums[i]<<" & nums[j]= "<<nums[j]<< endl;
-            }
+            // 4. Reverse all elements towards right of index1
+            reverse(nums.begin() + (index1 + 1), nums.end());
             
         }
     }
