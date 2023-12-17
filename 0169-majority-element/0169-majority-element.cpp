db3f905a8d59683b9b60bf81ae9530b4bc9fe8ad
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-12-17 17:06:57 +0530

    Time: 11 ms (84.59%), Space: 20.3 MB (11.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -13,6 +13,7 @@ public:
         // return nums[0];
         
         // Boyer-Moore Voting Algorithm
+        // https://www.youtube.com/watch?v=nP_ns3uSh80&list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB&index=7
 //         int count = 0;
 //         int majorityElement = 0;
         
@@ -28,6 +29,7 @@ public:
 //         return majorityElement;
         
         // Median of array also O(N) complexity same as Boyer-Moore 
+        // Explanation: https://leetcode.com/problems/majority-element/discuss/1427775/2-line-easy-and-clean-C%2B%2B-solution-(explained)
         nth_element(nums.begin(), nums.begin() + nums.size()/2, nums.end());
         return nums[nums.size()/2];
     }
