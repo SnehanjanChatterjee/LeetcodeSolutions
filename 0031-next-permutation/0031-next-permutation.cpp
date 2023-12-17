23025a6f7c19abe562286f80914269aa9c059154
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-12-17 22:50:57 +0530

    Time: 0 ms (100.00%), Space: 12.4 MB (54.25%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -8,18 +8,18 @@ public:
         for(int i=n-2; i>=0; i--) {
             if(nums[i]<nums[i+1]) {
                 index1 = i;
-                cout << "Found index1 = " << index1 << endl;
+                // cout << "Found index1 = " << index1 << endl;
                 break;
             }
         }
         if (index1 == -1) {
             sort(nums.begin(), nums.end());
-            cout << "Not Found index1 = " << index1 << ", so sorted array" << endl;
+            // cout << "Not Found index1 = " << index1 << ", so sorted array" << endl;
         } else {
             for(int i=n-1; i>index1; i--) {
                 if(nums[i]>nums[index1]) {
                     index2 = i;
-                    cout << "Found index2 = " << index2 << endl;
+                    // cout << "Found index2 = " << index2 << endl;
                     break;
                 }
             }
@@ -28,13 +28,13 @@ public:
             nums[index1] = nums[index2];
             nums[index2] = temp;
 
-            cout<<"Swapped nums[index1]= "<<nums[index1]<<" & nums[index2]= "<<nums[index2]<< endl;
+            // cout<<"Swapped nums[index1]= "<<nums[index1]<<" & nums[index2]= "<<nums[index2]<< endl;
 
             for(int i=index1+1, j=n-1;i<j;i++, j--) {
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                cout<<"Swapped nums[i]= "<<nums[i]<<" & nums[j]= "<<nums[j]<< endl;
+                // cout<<"Swapped nums[i]= "<<nums[i]<<" & nums[j]= "<<nums[j]<< endl;
             }
             
         }
