fd13c61f7ecf77b82adc859ad5f3c6b4b7b3e96c
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-12-18 01:01:56 +0530

    Time: 0 ms (100.00%), Space: 12.4 MB (54.25%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -8,12 +8,15 @@ public:
         
         // 1. Find index1 such that nums[i] < nums[i+1]
         for(int i = n - 2; i >= 0; i--) {
-            if(nums[i]<nums[i+1]) {
+            if(nums[i] < nums[i+1]) {
                 index1 = i;
                 // cout << "Found index1 = " << index1 << endl;
                 break;
             }
         }
+        
+        // If initial array was in descending order, so index1 will be -1. 
+        // So next permutation will be reverse of original array i.e sorted array
         if (index1 == -1) {
             sort(nums.begin(), nums.end());
             // cout << "Not Found index1 = " << index1 << ", so sorted array" << endl;
@@ -21,7 +24,7 @@ public:
             
             // 2. Find index2 toward right of index1 such that nums[i] > nums[index1]
             for(int i = n - 1; i > index1; i--) {
-                if(nums[i]>nums[index1]) {
+                if(nums[i] > nums[index1]) {
                     index2 = i;
                     // cout << "Found index2 = " << index2 << endl;
                     break;
