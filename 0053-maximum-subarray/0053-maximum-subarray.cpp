3e5f8a409a1bc962bb8a96f78e0797bd9a7ef594
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-12-17 17:47:03 +0530

    Time: 92 ms (22.67%), Space: 70.7 MB (6.72%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        // DP Solution
+        int n = nums.size();
+        vector<int> dp(n);//dp[i] means the maximum subarray ending with A[i];
+        dp[0] = nums[0];
+        int maxElement = dp[0];
+        
+        for(int i = 1; i < n; i++){
+            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            maxElement = max(maxElement, dp[i]);
+        }
+        
+        // Const space soln, time -> O(N)
+        // Kadane's Algorithm
+        // int sum = 0;
+        // int maxElement = nums[0];
+        // for(int x: nums) {
+        //     sum += x;
+        //     maxElement = max(maxElement, sum);
+        //     if(sum < 0) {
+        //         sum = 0;
+        //     }
+        // }
+        
+        return maxElement;
+    }
+};
