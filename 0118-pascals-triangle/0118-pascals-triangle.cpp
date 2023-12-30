5422be09cb967600d582ad9826772be4adceb335
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-12-30 21:11:38 +0530

    Time: 6 ms (26.50%), Space: 7.1 MB (80.13%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    // https://www.youtube.com/watch?v=bR7mQgwQ_o8&list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB&index=18
+    vector<int> generateRow(int row) {
+        long long ans = 1;
+        vector<int> ansRow;
+        ansRow.push_back(1); //inserting the 1st element
+
+        //calculate the rest of the elements:
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
+    vector<vector<int>> solution2(int n) {
+        vector<vector<int>> ans;
+
+        //store the entire pascal's triangle:
+        for (int row = 1; row <= n; row++) {
+            ans.push_back(generateRow(row));
+        }
+        return ans;
+    }
+    
+    vector<vector<int>> mySolution(int n) {
+        vector<vector<int>> ans(n);
+        
+        for(int i=0; i<n; i++) {
+            ans[i].resize(i+1);
+            for(int j=0; j<i+1; j++) {
+                if(j == 0 || j == i) ans[i][j] = 1;
+                else ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+            }
+        }
+        
+        return ans;
+    }
+public:
+    vector<vector<int>> generate(int numRows) {
+        // return mySolution(numRows);
+        
+        return solution2(numRows);
+    }
+};
