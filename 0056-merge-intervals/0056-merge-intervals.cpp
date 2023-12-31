74fbe0c1d88a57dab9f6f6b51549b29a46a63704
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2023-12-31 19:19:26 +0530

    Time: 45 ms (13.04%), Space: 19.5 MB (43.11%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,6 +1,26 @@
 class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+private:
+    // Not using any variable
+    vector<vector<int>> soln1(vector<vector<int>>& intervals) {
+        vector<vector<int>> merged;
+        int n=intervals.size();
+        
+        sort(intervals.begin(), intervals.end());
+        merged.push_back(intervals[0]);
+        
+        for(int i=0;i<n;i++) {
+            if(merged.back()[1] >= intervals[i][0]) {
+                merged.back()[0] = min(intervals[i][0], merged.back()[0]);
+                merged.back()[1] = max(intervals[i][1], merged.back()[1]);
+            } else {
+                merged.push_back(intervals[i]);
+            }
+        }
+        return merged;
+    }
+    
+    // Using variable
+    vector<vector<int>> soln2(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
         
         sort(intervals.begin(), intervals.end());
@@ -19,5 +39,11 @@ public:
         ans.push_back({start, end});
         
         return ans;
+    }
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // return soln1(intervals);
+        
+        return soln2(intervals);
     }
 };
