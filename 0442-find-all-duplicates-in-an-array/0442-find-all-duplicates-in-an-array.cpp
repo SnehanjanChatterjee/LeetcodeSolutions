079d5d502110e96e4479d4aa99a2a350ee866439
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-03-25 19:10:31 +0530

    Time: 31 ms (94.91%), Space: 36 MB (68.64%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,18 +1,42 @@
 class Solution {
-public:
-    vector<int> findDuplicates(vector<int>& nums) {
-        map<int, int> mp;
-        
-        for(int &x: nums) mp[x]++;
-        
+private:
+    vector<int> mySolution(vector<int>& nums) {
+        unordered_map<int, int> ump;
+
+        for(int &x: nums) ump[x]++;
+
         vector<int> ans;
-        
-        for(auto &x: mp) {
+
+        for(auto &x: ump) {
             if(x.second > 1) {
                 ans.push_back(x.first);
             }
         }
+
+        return ans;
+    }
+    
+    vector<int> optimizedSolution(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        
+        // Since in the array there are elements from 1-n, (nums[i]-1) will always be a valid index.
+        // So if there are duplicate numbers, (nums[i]-1) will point to same index
+        // We can keep marking every (nums[i]-1)th value as negative so that if we get a negative number,
+        // we would know someone with same value marked this num as negative
+        for(int i = 0; i<n; i++) {
+            int index = abs(nums[i]) - 1;
+            if(nums[index] < 0) ans.push_back(abs(nums[i]));
+            nums[index] = -(nums[index]);
+        }
         
         return ans;
+    }
+public:
+    // If we are said to return a container, that doesn't count as extra space
+    vector<int> findDuplicates(vector<int>& nums) {
+        // return mySolution(nums);
+        
+        return optimizedSolution(nums);
     }
 };
