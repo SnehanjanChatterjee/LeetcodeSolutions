12155ad49524c77f350d47b52848b28b4e1d33fb
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-03-25 17:18:55 +0530

    Time: 607 ms (20.43%), Space: 287.4 MB (12.35%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -63,6 +63,10 @@ private:
     // SC -> O(N)
     // Ref -> 
     // 1. https://github.com/gibsjose/cpp-cheat-sheet/blob/master/Data%20Structures%20and%20Algorithms.md#44-merge-sort
+    // 2. // TC -> O(NlogN) for the all cases
+    // SC -> O(N)
+    // Ref -> 
+    // 1. https://github.com/gibsjose/cpp-cheat-sheet/blob/master/Data%20Structures%20and%20Algorithms.md#44-merge-sort
     // 2. https://www.youtube.com/watch?v=HGk_ypEuS24
     void mergeSort(vector<int>& nums, int low, int high) {
         if (low >= high) return;
@@ -106,6 +110,36 @@ private:
         }
     }
     
+    // TC -> O(NlogN) for the best and average cases and O(N^2) for the worst case (Already sorted)
+    // SC -> O(N)
+    // Ref -> 
+    // 1. https://github.com/gibsjose/cpp-cheat-sheet/blob/master/Data%20Structures%20and%20Algorithms.md#45-quicksort
+    // 2. https://www.youtube.com/watch?v=WIrA4YexLRQ
+    void quickSort(vector<int> &nums, int low, int high) {
+        if (low < high) {
+            int pIndex = partition(nums, low, high);
+            quickSort(nums, low, pIndex - 1);
+            quickSort(nums, pIndex + 1, high);
+        }
+    }
+    int partition(vector<int> &nums, int low, int high) {
+        int pivot = nums[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (nums[i] <= pivot && i <= high - 1) {
+                i++;
+            }
+
+            while (nums[j] > pivot && j >= low + 1) {
+                j--;
+            }
+            if (i < j) swap(nums[i], nums[j]);
+        }
+        swap(nums[low], nums[j]);
+        return j;
+    }
     
 public:
     vector<int> sortArray(vector<int>& nums) {
@@ -113,6 +147,7 @@ public:
         // bubbleSort(nums);
         // insertionSort(nums);
         mergeSort(nums, 0, nums.size() - 1);
+        // quickSort(nums, 0, nums.size() - 1);
         
         return nums;
     }
