12abe4615f8054e0a6cfa9bd72d5d3b1d3165da5
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-03-26 11:36:46 +0530

    Time: 50 ms (33.00%), Space: 43.5 MB (95.93%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -13,11 +13,15 @@ private:
     }
     
     // https://leetcode.com/problems/first-missing-positive/discuss/17071/My-short-c%2B%2B-solution-O(1)-space-and-O(n)-time
+    // Put each number in its right place. For example: When we find 5, then swap it with A[4].
+    // At last, the first place where its number is not right, return the place + 1.
+    // For negative numbers, it will get swapped to a wrong position.
     int optimizedSolution(vector<int>& nums) {
         int n = nums.size(); 
         for (int i = 0; i < n; i++)
             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
                 swap(nums[i], nums[nums[i] - 1]);
+        
         for (int i = 0; i < n; i++)
             if (nums[i] != i + 1)
                 return i + 1;
