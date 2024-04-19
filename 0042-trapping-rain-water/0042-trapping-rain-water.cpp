8ed177bf8388b5d8c1183b651798126b64c58d88
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-04-20 02:44:22 +0530

    Time: 11 ms (53.54%), Space: 22.3 MB (82.42%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // Striver video: https://www.youtube.com/watch?v=m18Hntz4go8&list=PLgUwDviBIf0rBT8io74a95xT-hDFZonNs&index=3
     int trap(vector<int>& height) {
         
         // Sliding window approach
@@ -7,27 +8,23 @@ public:
         // s --> O(1)
         int n = height.size(); 
         int left = 0, right = n-1;
-        int res = 0;
         int maxleft = 0, maxright = 0;
+        int res = 0;
         
         while(left<=right){
             
             // So there exist an elevation of greater ht on right than current elevation
-            if(height[left] <= height[right]){
+            if (height[left] <= height[right]) {
 
-                if(height[left]>=maxleft)
-                    maxleft = height[left];
-                else if(height[left]<maxleft)
-                    res += maxleft-height[left];
+                if(height[left]>=maxleft) maxleft = height[left];
+                else if(height[left]<maxleft) res += maxleft-height[left];
                 
                 left++;
             }
-            else if(height[left] > height[right]){
+            else if (height[left] > height[right]) {
 
-                if(height[right]>=maxright)
-                    maxright = height[right];
-                else if(height[right]<maxright)
-                    res += maxright-height[right];
+                if(height[right]>=maxright) maxright = height[right];
+                else if(height[right]<maxright) res += maxright-height[right];
                 
                 right--;
             }
