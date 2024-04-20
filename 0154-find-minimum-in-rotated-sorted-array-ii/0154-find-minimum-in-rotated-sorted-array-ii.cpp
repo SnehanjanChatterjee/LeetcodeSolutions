b4d1c512b64fc6288f45d608d592653891e7650a
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-04-21 00:18:23 +0530

    Time: 3 ms (81.21%), Space: 14.7 MB (72.23%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    // https://www.youtube.com/watch?v=nhEMDKMB44g&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=7
+    int striverSoln(vector<int>& nums, int n) {
+        int low = 0;
+        int high = n-1;
+        int mid;
+        int ans = INT_MAX;
+        
+        while (low <= high) {
+            mid = low + ((high - low) / 2);
+            
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                ans = min(ans, nums[mid]);
+                low++;
+                high--;
+                continue;
+            }
+            
+            // Left half of mid is sorted
+            if (nums[low] <= nums[mid]) {
+                // Since left half is sorted,
+                // we can directly check the first element of left half (i.e nums[left]) if it's the minimum value
+                // & eliminate 1st half (we don't need to go into the 1st half as in a sorted space 1st element can only be minimum)
+                ans = min(ans, nums[low]);
+                low = mid + 1;
+            }
+            // Right half of mid is sorted
+            else {
+                ans = min(ans, nums[mid]);
+                high = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+public:
+    int findMin(vector<int>& nums) {
+        return striverSoln(nums, nums.size());
+    }
+};
