be32c123e008a6b8ac189c98ef914f0b074f29de
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-04-21 19:51:04 +0530

    Time: 3 ms (71.72%), Space: 12 MB (78.97%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,5 +1,7 @@
 class Solution {
 private:
+    // TC -> O(N)
+    // SC -> O(1)
     int striver_BruteForce(vector<int>& arr, int k) {
         // Array elements can lie b/w 1 and 1000
         // If there is an element x whose value is <= k, that means x is taking a place in between 1 & kth missing no. So k++
@@ -11,9 +13,33 @@ private:
         }
         return k;
     }
+    
+    // TC -> O(logN)
+    // SC -> O(N)
+    int striver_Optimal(vector<int>& arr, int k) {
+        int n = arr.size();
+        int low = 0, high = n - 1, mid, missing;
+        
+        while (low <= high) {
+            mid = low + ((high - low) / 2);
+            missing = arr[mid] - (mid + 1);
+            
+            if (missing < k) low = mid + 1;
+            else high = mid - 1;
+        }
+        
+        // ans -> arr[high] + some more value
+        // ans -> arr[high] + (k - missing)
+        // ans -> arr[high] + (k - (arr[high] - (high + 1)))
+        // ans -> arr[high] + k - arr[high] + high + 1
+        // ans -> (high + 1) + k
+        // ans -> low + k
+        return low + k;
+    }
 public:
     // https://www.youtube.com/watch?v=uZ0N_hZpyps&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=17
     int findKthPositive(vector<int>& arr, int k) {
-        return striver_BruteForce(arr, k);
+        // return striver_BruteForce(arr, k);
+        return striver_Optimal(arr, k);
     }
 };
