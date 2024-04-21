f68527091dc35b75cf79d5249c4842a3ee1c0710
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-04-21 05:49:33 +0530

    Time: 91 ms (88.35%), Space: 59.2 MB (98.06%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,7 +1,81 @@
 class Solution {
-public:
+private:
+    vector<int> dRow = {-1, 0, +1, 0};
+    vector<int> dCol = {0, +1, 0, -1};
+    
+    void printVector(vector<int> &group) {
+        for (auto &x: group) cout << x << ' ';
+        cout << endl;
+    }
+    
+    bool isWithinGrid(int row, int col, int rows, int cols) {
+        return (row >= 0 && row < rows && col >= 0 && col < cols);
+    }
+    
+    void dfs(int row, int col, int rows, int cols, vector<vector<int>>& land, vector<vector<int>>& vis, vector<int>& group) {
+        if (!isWithinGrid(row, col, rows, cols)) return;
+        
+        vis[row][col] = 1;
+        
+        // Last cell
+        if (row == (rows - 1) && col == (cols - 1)) {
+            group.push_back(row);
+            group.push_back(col);
+        }
+        
+        // Both right and bottom exist but both are 0
+        else if (col + 1 < cols && land[row][col + 1] == 0 && row + 1 < rows && land[row + 1][col] == 0) {
+            group.push_back(row);
+            group.push_back(col);
+        }
+        
+        // Bottom doesn't exist & right is 0        
+        else if (row == rows - 1 && land[row][col + 1] == 0) {
+            group.push_back(row);
+            group.push_back(col);
+        }
+        
+        // Right doesn't exist & bottom is 0
+        else if (col == cols - 1 && land[row + 1][col] == 0) {
+            group.push_back(row);
+            group.push_back(col);
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            int nRow = row + dRow[i];
+            int nCol = col + dCol[i];
+            if (isWithinGrid(nRow, nCol, rows, cols) && (land[nRow][nCol] == 1 && vis[nRow][nCol] == 0)) {
+                dfs(nRow, nCol, rows, cols, land, vis, group);
+            }
+        }
+    }
+    // Ref: https://www.youtube.com/watch?v=dCNPK13QCP8
+    vector<vector<int>> mySolution(vector<vector<int>>& land) {
+        int rows = land.size();
+        int cols = land[0].size();
+        vector<vector<int>> ans;
+        vector<vector<int>> vis(rows, vector<int> (cols, 0));
+        
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (land[row][col] == 1 && vis[row][col] == 0) {
+                    vector<int> group;
+                    
+                    group.push_back(row);
+                    group.push_back(col);
+                    
+                    dfs(row, col, rows, cols, land, vis, group);
+                    
+                    ans.push_back(group);
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
     // https://leetcode.com/problems/find-all-groups-of-farmland/discuss/1444086/C%2B%2B-Straight-forward-approach-no-super-mind-(Commented)
-    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+    vector<vector<int>> discussionSolution(vector<vector<int>>& land) {
         int m = land.size();
         int n = land[0].size();
         
@@ -43,5 +117,11 @@ public:
             }
         }
         return result;
+    }
+public:
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        // return mySolution(land);
+        
+        return discussionSolution(land);
     }
 };
