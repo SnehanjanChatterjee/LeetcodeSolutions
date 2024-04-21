da2d3c92bbfb980ac7a36a8548973e51af6a0c0c
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-04-21 14:44:19 +0530

    Time: 37 ms (29.50%), Space: 21.4 MB (30.33%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -11,7 +11,11 @@ private:
     }
 public:
     // https://www.youtube.com/watch?v=qyfekrNni90&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=13
-    int minEatingSpeed(vector<int>& piles, int h) {        
+    int minEatingSpeed(vector<int>& piles, int h) {
+        // Here we are doing binary search on answer space (not in the given input space)
+        // The ans can lie b/w 1 and max element of array ->
+        // Total time taken to eat maxElement bananas per hour, will be same as time taken to eat maxElement+1 bananas per hour.
+        // So max search space can be maxElement of array.
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
         int mid;
