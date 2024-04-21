67dccbbcbe47435eecac07f5e15574f21b90417d
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-04-21 19:25:11 +0530

    Time: 2 ms (74.18%), Space: 12 MB (78.97%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    int striver_BruteForce(vector<int>& arr, int k) {
+        // Array elements can lie b/w 1 and 1000
+        // If there is an element x whose value is <= k, that means x is taking a place in between 1 & kth missing no. So k++
+        // E.g [5,7,10,12], k = 6, Missing Nos = [1, 2, 3, 4, 5(not missing but takes plae in between), 6].
+        // So 6ht missing will be k + 1 = 7.
+        for (const int &x : arr) {
+            if(x <= k) k++;
+            else break;
+        }
+        return k;
+    }
+public:
+    // https://www.youtube.com/watch?v=uZ0N_hZpyps&list=PLgUwDviBIf0pMFMWuuvDNMAkoQFi-h0ZF&index=17
+    int findKthPositive(vector<int>& arr, int k) {
+        return striver_BruteForce(arr, k);
+    }
+};
