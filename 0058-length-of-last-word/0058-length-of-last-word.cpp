d26a0b19e325a9e9eb995cce127c64dc4e2449a6
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-04-28 17:52:33 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (18.59%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int cnt = 0, n = s.size();
+        int cnt = 0;
+        int n = s.size();
+        
         for (int i = n - 1; i >= 0; i--) {
-            if (cnt == 0 && s[i] == ' ') continue;
-            else if (s[i] == ' ') return cnt;
-            else cnt++;
-            
-            cout << "cnt = " << cnt << " for s["<<i<<"] = " << s[i] << endl;
+            if (cnt > 0 && s[i] == ' ') return cnt;
+            else if (s[i] != ' ') cnt++;
         }
         
         return cnt;
