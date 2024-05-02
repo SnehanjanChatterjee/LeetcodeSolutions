d11fbe815979dd80aab9b8107a28e187f555d0f1
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-05-03 03:01:31 +0530

    Time: 9 ms (96.16%), Space: 22.9 MB (65.72%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,6 +1,20 @@
 class Solution {
-public:
-    int findMaxK(vector<int>& nums) {
+private:
+    int using2Pointer(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int low = 0, high = nums.size() - 1;
+        
+        while(low <= high)
+        {
+            if((nums[low] + nums[high]) == 0) return  nums[high];
+            else if((nums[low] + nums[high]) < 0) low++;
+            else high--;
+        }
+        
+        return -1;
+    }
+    int usingSet(vector<int>& nums) {
         unordered_set<int> ust;
         int maxK = -1;
         
@@ -12,5 +26,10 @@ public:
         }
         
         return maxK;
+    }
+public:
+    int findMaxK(vector<int>& nums) {
+        // return usingSet(nums);
+        return using2Pointer(nums);
     }
 };
