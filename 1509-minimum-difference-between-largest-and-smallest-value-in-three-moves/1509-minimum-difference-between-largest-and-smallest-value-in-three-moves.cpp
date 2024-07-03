bd38e4fdd06f084deaa84912ae89c730f11dd7c8
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2024-07-03 19:48:08 +0530

    Time: 79 ms (11.77%), Space: 38.8 MB (15.52%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,7 +1,7 @@
 class Solution {
-public:
+private:
     // https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/discuss/5406010/3-Solutions-Tutorial-O(n)-Included-Minimum-Difference-Between-Largest-and-Smallest-Value
-    int minDifference(vector<int>& nums) {
+    int leetcodeDiscussionSoln(vector<int>& nums) {
         if(nums.size() <= 4) {
             return 0;
         }
@@ -11,5 +11,27 @@ public:
             ans = min(ans, nums[nums.size() - (3 - i) - 1] - nums[i]);
         }
         return ans;
+    }
+    
+    // https://www.youtube.com/watch?v=HQix60wTKPc
+    int frazSoln(vector<int>& nums) {
+        int n = nums.size();
+        
+        if(n <= 4) return 0;
+        
+        sort(nums.begin(), nums.end());
+        
+        int op1 = nums[n-4] - nums[0]; // Make last 3 elements same as 1st element. So now max element is (n-4)th element
+        int op2 = nums[n-3] - nums[1]; // Make 1st 2 & last 2 elements same. So now max element is (n-3)th element
+        int op3 = nums[n-2] - nums[2]; // Make 1st 3 & last elements same. So now max element is (n-2)th element
+        int op4 = nums[n-1] - nums[3]; // Make 1st 3 elements same as last element. So now max element is (n-1)th element
+        
+        return min(op1, min(op2, min(op3, op4)));
+    }
+public:
+    int minDifference(vector<int>& nums) {
+        // return leetcodeDiscussionSoln(nums);
+        
+        return frazSoln(nums);
     }
 };
